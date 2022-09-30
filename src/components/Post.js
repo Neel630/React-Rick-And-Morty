f1739fddea053d1aea9306767ad19ac92bf0e1c5
sunny unde <sunnyunde07@gmail.com>
2022-09-30 14:24:26 +0000

    Error Handled properly

--- src/components/Post.js
+++ src/components/Post.js
@@ -7,7 +7,7 @@ const Post = ({ posts, loading }) => {
 
   return (
     <ul className="list-group mb-4">
-      {posts.map(post => (
+      {posts.map((post) => (
         <li key={post.id} className="list-group-item">
           <h4>{post.episode}</h4>
           <h5>{post.name}</h5>
